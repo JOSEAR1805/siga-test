e2fcc9a1ddb89c14659c47f64513783b098b5fef
Yen Colon <ycolon@silta.com>
2021-07-30 23:14:56 -0400

    graphic works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,11 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import { Line } from '@ant-design/charts';
+import dynamic from 'next/dynamic'
 
+const Chart = dynamic(() => import('../components/charts/index'), { ssr: false })
 
 export default function Home() {
-    const data = [
-        { year: '1991', value: 3 },
-        { year: '1992', value: 4 },
-        { year: '1993', value: 3.5 },
-        { year: '1994', value: 5 },
-        { year: '1995', value: 4.9 },
-        { year: '1996', value: 6 },
-        { year: '1997', value: 7 },
-        { year: '1998', value: 9 },
-        { year: '1999', value: 13 },
-      ];
-    
-      const config = {
-        data,
-        height: 400,
-        xField: 'year',
-        yField: 'value',
-        point: {
-          size: 5,
-          shape: 'diamond',
-        },
-        label: {
-          style: {
-            fill: '#aaa',
-          },
-        },
-      };
 
   return (
     <div className={styles.container}>
@@ -41,7 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Line {...config} />
+      {/* <Line {...config} /> */}
+      {/* <Crazy /> */}
+      <Chart />
 
       <footer className={styles.footer}>
         <a
